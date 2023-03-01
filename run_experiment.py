e56f9af22330a8d033f2f046588492ee6dfe1141
Nils Lehmann <nils.lehmann24@gmail.com>
2023-03-01 10:23:01 +0100

    dummy change

--- run_experiment.py
+++ run_experiment.py
@@ -10,6 +10,7 @@ from experiments.setup_experiment import (
 )
 from experiments.utils import create_experiment_dir, read_config, save_config
 
+# chang here
 
 def run(config_path: str):
     """Training and Evaluation Script.
