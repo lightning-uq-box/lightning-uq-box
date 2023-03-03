9eb9fa1c4d5c37f5c0ff378b117c5635df2d0313
Nils Lehmann <nils.lehmann24@gmail.com>
2023-03-03 14:45:16 +0100

    better file name for gaussian deterministic model

--- uq_method_box/uq_methods/__init__.py
+++ uq_method_box/uq_methods/__init__.py
@@ -3,7 +3,7 @@
 from .base import BaseModel, EnsembleModel
 from .cqr_model import CQR
 from .deep_ensemble_model import DeepEnsembleModel
-from .gaussian_nll import DeterministicGaussianModel
+from .deterministic_gaussian import DeterministicGaussianModel
 from .laplace_model import LaplaceModel
 from .mc_dropout_model import MCDropoutModel
 from .quantile_regression_model import QuantileRegressionModel
