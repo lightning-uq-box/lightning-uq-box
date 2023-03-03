e3a95881bd7e14fc1dcd202220a2102e792b4394
nm19000 <118897012+nm19000@users.noreply.github.com>
2023-03-03 17:20:54 +0100

    my changes

--- uq_method_box/uq_methods/sgld_nll.py
+++ uq_method_box/uq_methods/sgld_nll.py
@@ -1,6 +1,6 @@
 """Stochastic Gradient Langevin Dynamics (SGLD) model."""
 
-from typing import Any, Dict
+from typing import Any, Dict, List
 
 import numpy as np
 import torch
@@ -9,34 +9,30 @@ from torch.optim import Optimizer
 from torch import Tensor
 from torch.utils.data import DataLoader
 
-from typing import Any, Dict, List
+from uq_method_box.eval_utils import (
+    compute_aleatoric_uncertainty,
+    compute_epistemic_uncertainty,
+    compute_predictive_uncertainty,
+    compute_quantiles_from_std
+)
 
 from uq_method_box.uq_methods import BaseModel
 from uq_method_box.train_utils import NLL
 
-#TO DOs:
-# change optimizer for training to make gradient updates: 
-"""Implementation of SGLD algorithm as described in [1].  The loss is assumed to be the average minibatch loss, hence
-  of the form
-      -(1/m) \sum_{i=1}^m log p(y_i|x_i,w) - (1/n) log p(w),
-  where m is the batch size and n is the total number of iid training samples.
-  #### References
-  [1] _Max Welling_ and _Yee Whye Teh_, NOTYPO
-      "bayesian Learning via Stochastic Gradient Langevin Dynamics",
-      ICML 2011,
-      [PDF](https://www.ics.uci.edu/~welling/publications/papers/stoclangevin_v6.pdf)  """
-
-
-"""SGLD optimizer class from: https://pysgmcmc.readthedocs.io/en/pytorch/_modules/pysgmcmc/optimizers/sgld.html """
-# Pytorch Port of a previous tensorflow implementation in `tensorflow_probability`:
-# https://github.com/tensorflow/probability/blob/master/tensorflow_probability/g3doc/api_docs/python/tfp/optimizer/StochasticGradientLangevinDynamics.md
 
 class SGLD(Optimizer):
-    """ Stochastic Gradient Langevin Dynamics Sampler with preconditioning.
+    """Stochastic Gradient Langevin Dynamics Sampler with preconditioning, as in https://pysgmcmc.readthedocs.io/en/pytorch/_modules/pysgmcmc/optimizers/sgld.html.
         Optimization variable is viewed as a posterior sample under Stochastic
-        Gradient Langevin Dynamics with noise rescaled in eaach dimension
-        according to RMSProp.
-    """
+        Gradient Langevin Dynamics with noise rescaled in each dimension
+        according to RMSProp. Implementation of SGLD algorithm as described in [1].  The loss is assumed to be the average minibatch loss, hence
+        of the form
+      -(1/m) \sum_{i=1}^m log p(y_i|x_i,w) - (1/n) log p(w),
+        where m is the batch size and n is the total number of iid training samples.
+        #### References
+        [1] _Max Welling_ and _Yee Whye Teh_, NOTYPO
+        "Bayesian Learning via Stochastic Gradient Langevin Dynamics",ICML 2011,
+        [PDF](https://www.ics.uci.edu/~welling/publications/papers/stoclangevin_v6.pdf)  """
+
 
     def __init__(self,
                  params,
@@ -47,34 +43,20 @@ class SGLD(Optimizer):
                  diagonal_bias=1e-8) -> None:
         """ Set up a SGLD Optimizer.
 
-        Parameters
-        ----------
-        params : iterable
-            Parameters serving as optimization variable.
-        lr : float, optional
-            Base learning rate for this optimizer.
-            Must be tuned to the specific function being minimized.
-            Default: `1e-2`.
-        precondition_decay_rate : float, optional
-            Exponential decay rate of the rescaling of the preconditioner (RMSprop).
-            Should be smaller than but nearly `1` to approximate sampling from the posterior.
-            Default: `0.95`
-        num_pseudo_batches : int, optional
-            Effective number of minibatches in the data set.
-            Trades off noise and prior with the SGD likelihood term.
-            Note: Assumes loss is taken as mean over a minibatch.
-            Otherwise, if the sum was taken, divide this number by the batch size.
-            Default: `1`.
-        num_burn_in_steps : int, optional
-            Number of iterations to collect gradient statistics to update the
-            preconditioner before starting to draw noisy samples.
-            Default: `3000`.
-        diagonal_bias : float, optional
+        Args: 
+            params : iterable Parameters serving as optimization variable.
+            lr : float, optional, Default: `1e-2`.
+            precondition_decay_rate : float, optional. Exponential decay rate of the rescaling of the preconditioner (RMSprop). Should be smaller than but nearly `1` to approximate sampling from the posterior. Default: `0.95`
+            num_pseudo_batches : int, optional. Effective number of minibatches in the data set. Trades off noise and prior with the SGD likelihood term. Note: Assumes loss is taken as mean over a minibatch. Otherwise, if the sum was taken, divide this number by the batch size. Default: `1`.
+            num_burn_in_steps : int, optional. Number of iterations to collect gradient statistics to update the preconditioner before starting to draw noisy samples. Default: `120`.
+            diagonal_bias : float, optional
             Term added to the diagonal of the preconditioner to prevent it from
             degenerating.
             Default: `1e-8`.
+        Returns:
+            -
+       """
 
-        """
         if lr < 0.0:
             raise ValueError("Invalid learning rate: {}".format(lr))
         if num_burn_in_steps < 0:
@@ -91,6 +73,15 @@ class SGLD(Optimizer):
 
 
     def step(self, closure=None):
+        """Compute updated loss, .
+
+        Args: 
+            self: model
+            closure: loss
+        Returns:
+            loss: updated loss
+       
+        """
         loss = None
 
         if closure is not None:
@@ -160,11 +151,13 @@ class SGLDModel(BaseModel):
     ) -> None:
         super().__init__(config, model, criterion)
         
-        self.n_burnin_pochs = self.config["model"]["base_model"]["n_burnin_pochs"]
+        self.n_burnin_pochs = self.config["model"]["base_model"]["n_burnin_epochs"]
         self.n_sgld_samples = self.config["model"]["base_model"]["n_sgld_samples"]
-        self.max_poch=self.config["pl"]["max_epoch"] 
-        #do I need to define this? 
-        self.Models = List[nn.Module]
+        self.max_poch=self.config["pl"]["max_epoch"]  
+        self.models: List[nn.Module] = []
+        self.quantiles = self.config["model"]["quantiles"]
+
+        assert( self.n_sgld_samples + self.n_burnin_epochs <= self.config["pl"]["max_epoch"]), "The number of max epochs needs to be larger than the sum of the burnin phase and model gathering"
 
         
     def configure_optimizers(self) -> Dict[str, Any]:
@@ -172,7 +165,7 @@ class SGLDModel(BaseModel):
 
         Returns:
             a "lr dict" according to the pytorch lightning documentation --
-            https://pytorch-lightning.readthedocs.io/en/latest/common/lightning_module.html#configure-optimizers
+            https://pytorch-lightning.readthedocs.io/en/latest/common/lightning_module.html#configure-optimizers, with SGLD optimizer.
         """
         optimizer = SGLD(self.model,
             self.model.parameters(), lr=self.config["optimizer"]["lr"]
@@ -186,22 +179,11 @@ class SGLDModel(BaseModel):
             batch: the output of your DataLoader
 
         Returns:
-            training loss and Models (can I do this?)
+            training loss and List of models 
         """
-        #need something to interrupt training step after n_burnin_epochs
-        #need to check if self.config["pl"]["max_epoch"] >= n_sgld_samples + n_burnin_epochs
-        #the another n_sgld_samples runs and save weights for ensemble
-     
-        if self.config["pl"]["max_epoch"] >= self.n_sgld_samples + self.n_burnin_epochs:
-            #no error, continue
-        else:
-            #send error message/print error
-            #return print("Too little epochs ")
-            
-        #does this work?
 
         if self.current_epoch > self.n_burnin_epochs: 
-            self.Models.append(copy.deepcopy(self.model))
+            self.models.append(copy.deepcopy(self.model))
 
         X, y = args[0]
         out = self.forward(X)
@@ -210,16 +192,7 @@ class SGLDModel(BaseModel):
         self.log("train_loss", loss)  # logging to Logger
         self.train_metrics(self.extract_mean_output(out), y)
 
-        return loss, self.Models
-
-    def training_epoch_end(self, outputs: Any) -> None:
-        """Log epoch-level training metrics.
-
-        Args:
-            outputs: list of items returned by training_step
-        """
-        self.log_dict(self.train_metrics.compute())
-        self.train_metrics.reset()
+        return loss, self.models
 
 
     def extract_mean_output(self, out: Tensor) -> Tensor:
@@ -241,12 +214,23 @@ class SGLDModel(BaseModel):
     def predict_step(
         self, batch: Any, batch_idx: int = 0, dataloader_idx: int = 0
     ) -> Dict[str, np.ndarray]:
-        """Predict step with SGLD ."""
-        #take n_sgld_sampled models in List Models obtained after n_burnin_epochs and corresponding learning rates (initially constant), get mean and variance
+        """Predict step with SGLD, take n_sgld_sampled models in list of models obtained after n_burnin_epochs and corresponding learning rates (initially constant), get mean and variance like for ensembles.
+        Args: 
+            self: SGLD class
+            batch_idx: default int=0
+            dataloader_idx: default int=0
+
+        Returns:
+            "mean": sgld_mean, mean prediction over models
+            "pred_uct": sgld_std, predictive uncertainty
+            "epistemic_uct": sgld_epistemic, epistemic uncertainty
+            "aleatoric_uct": sgld_aleatoric, aleatoric uncertainty, averaged over models
+            "quantiles": sgld_quantiles, quantiles assuming output is Gaussian
+        """
         
-                #pseudo code
+
         preds = (
-            torch.stack([self.model(batch) for self.model in self.Models], dim=-1)
+            torch.stack([self.model(batch) for self.model in self.models], dim=-1)
             .detach()
             .numpy()
         )  # shape [n_sgld_samples, batch_size, num_outputs]
@@ -260,11 +244,16 @@ class SGLDModel(BaseModel):
         sgld_aleatoric = compute_aleatoric_uncertainty(sigma_samples)
         sgld_epistemic = compute_epistemic_uncertainty(mean_samples)
 
+        #now compute quantiles, dimension is len(self.quantiles)
+
+        sgld_quantiles = compute_quantiles_from_std(sgld_mean, sgld_std, self.quantiles)
+
         return {
             "mean": sgld_mean,
             "pred_uct": sgld_std,
             "epistemic_uct": sgld_epistemic,
             "aleatoric_uct": sgld_aleatoric,
+            "quantiles": sgld_quantiles
         }
     
     
