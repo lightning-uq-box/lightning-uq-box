3dc4e1c2f944ae6eb8d0ac8f87683b3905e6f976
Nils Lehmann <nils.lehmann24@gmail.com>
2023-03-18 16:16:52 +0100

    restructure deep ensemble and write test for it

--- tests/uq_methods/test_base.py
+++ tests/uq_methods/test_base.py
@@ -1,10 +1,9 @@
-"""Unit tests for base class."""
+"""Unit tests for Base Model."""
 
 import os
 import sys
 from pathlib import Path
 
-import numpy as np
 import pytest
 import torch
 from lightning import Trainer
@@ -18,15 +17,15 @@ from omegaconf import OmegaConf
 
 from uq_method_box.datamodules import ToyHeteroscedasticDatamodule
 from uq_method_box.models import MLP
-from uq_method_box.uq_methods import BaseModel, EnsembleModel
+from uq_method_box.uq_methods import BaseModel
 
 
 class TestBaseModel:
     @pytest.fixture
     def base_model(self, tmp_path: Path) -> BaseModel:
+        """Create a base model being used for different tests."""
         conf = OmegaConf.load(os.path.join("tests", "configs", "base.yaml"))
         conf_dict = OmegaConf.to_object(conf)
-
         return BaseModel(
             MLP,
             model_args=conf_dict["model"]["model_args"],
@@ -47,8 +46,6 @@ class TestBaseModel:
         """Test Base Model with a Lightning Trainer."""
         # instantiate datamodule
         datamodule = ToyHeteroscedasticDatamodule()
-
-        # Instantiate trainer
         trainer = Trainer(
             log_every_n_steps=1,
             max_epochs=1,
@@ -56,76 +53,3 @@ class TestBaseModel:
         )
         trainer.fit(model=base_model, datamodule=datamodule)
         trainer.test(model=base_model, datamodule=datamodule)
-
-
-class TestBaseEnsembleModel:
-    @pytest.fixture
-    def ensemble_model(self, tmp_path: Path) -> EnsembleModel:
-        conf = OmegaConf.load(os.path.join("tests", "configs", "base_ensemble.yaml"))
-        conf_dict = OmegaConf.to_object(conf)
-
-        model_paths = []
-        for i in range(conf_dict["model"]["ensemble_members"]):
-            # set temporary save directory for logging
-            conf_dict["experiment"] = {}
-            save_path = os.path.join(tmp_path, f"ensemble_{i}")
-            conf_dict["experiment"]["save_dir"] = save_path
-
-            # instantiate datamodule
-            datamodule = ToyHeteroscedasticDatamodule()
-
-            model = BaseModel(
-                MLP,
-                model_args=conf_dict["model"]["model_args"],
-                lr=1e-3,
-                loss_fn="mse",
-                save_dir=save_path,
-            )
-
-            # Instantiate trainer
-            trainer = Trainer(
-                log_every_n_steps=1, max_epochs=1, default_root_dir=save_path
-            )
-            trainer.fit(model=model, datamodule=datamodule)
-
-            # save path to load them for ensemble
-            model_paths.append(
-                os.path.join(
-                    save_path,
-                    "lightning_logs",
-                    "version_0",
-                    "checkpoints",
-                    "epoch=0-step=1.ckpt",
-                )
-            )
-
-        # wrap Deep Ensemble Model
-        ensemble_members = []
-        for ckpt_path in model_paths:
-            ensemble_members.append({"model_class": BaseModel, "ckpt_path": ckpt_path})
-
-        # ensemble_dict = OmegaConf.to_object(conf)
-        # ensemble_dict["experiment"]["save_dir"] =
-
-        return EnsembleModel(
-            ensemble_members, save_dir=os.path.join(tmp_path, "prediction")
-        )
-
-    def test_ensemble_forward(self, ensemble_model: EnsembleModel) -> None:
-        """Test ensemble predict step."""
-        print(ensemble_model.hparams.ensemble_members)
-        X = torch.randn(5, 1)
-        out = ensemble_model.predict_step(X)
-        assert isinstance(out, dict)
-        assert isinstance(out["mean"], np.ndarray)
-        assert out["mean"].shape[0] == 5
-
-    def test_ensemble_trainer_test(self, ensemble_model: EnsembleModel) -> None:
-        """Test ensemble test step with lightning Trainer."""
-        datamodule = ToyHeteroscedasticDatamodule()
-        trainer = Trainer(
-            log_every_n_steps=1,
-            max_epochs=1,
-            default_root_dir=ensemble_model.hparams.save_dir,
-        )
-        trainer.test(ensemble_model, datamodule.test_dataloader())
---
+++ tests/uq_methods/test_deep_ensemble_model.py
@@ -0,0 +1,91 @@
+"""Unit tests Deep Ensemble Model."""
+
+import os
+import sys
+from pathlib import Path
+
+import numpy as np
+import pytest
+import torch
+from lightning import Trainer
+
+# required to make the path visible to import the tools
+# this will change in public notebooks to be "pip install uq-regression-box"
+if os.getcwd() not in sys.path:
+    sys.path.append(os.getcwd())
+
+from omegaconf import OmegaConf
+
+from uq_method_box.datamodules import ToyHeteroscedasticDatamodule
+from uq_method_box.models import MLP
+from uq_method_box.uq_methods import BaseModel, DeepEnsembleModel
+
+
+class TestBaseEnsembleModel:
+    @pytest.fixture
+    def ensemble_model(self, tmp_path: Path) -> DeepEnsembleModel:
+        """Create a Deep Ensemble Model being used for tests."""
+        conf = OmegaConf.load(os.path.join("tests", "configs", "deep_ensemble.yaml"))
+        conf_dict = OmegaConf.to_object(conf)
+
+        model_paths = []
+        for i in range(conf_dict["model"]["ensemble_members"]):
+            # set temporary save directory for logging
+            conf_dict["experiment"] = {}
+            save_path = os.path.join(tmp_path, f"ensemble_{i}")
+            conf_dict["experiment"]["save_dir"] = save_path
+
+            # instantiate datamodule
+            datamodule = ToyHeteroscedasticDatamodule()
+
+            model = BaseModel(
+                MLP,
+                model_args=conf_dict["model"]["model_args"],
+                lr=1e-3,
+                loss_fn="mse",
+                save_dir=save_path,
+            )
+
+            # Instantiate trainer
+            trainer = Trainer(
+                log_every_n_steps=1, max_epochs=1, default_root_dir=save_path
+            )
+            trainer.fit(model=model, datamodule=datamodule)
+
+            # save path to load them for ensemble
+            model_paths.append(
+                os.path.join(
+                    save_path,
+                    "lightning_logs",
+                    "version_0",
+                    "checkpoints",
+                    "epoch=0-step=1.ckpt",
+                )
+            )
+
+        ensemble_members = []
+        for ckpt_path in model_paths:
+            ensemble_members.append({"model_class": BaseModel, "ckpt_path": ckpt_path})
+
+        return DeepEnsembleModel(
+            ensemble_members, save_dir=os.path.join(tmp_path, "prediction")
+        )
+
+    def test_ensemble_forward(self, ensemble_model: DeepEnsembleModel) -> None:
+        """Test ensemble predict step."""
+        print(ensemble_model.hparams.ensemble_members)
+        X = torch.randn(5, 1)
+        out = ensemble_model.predict_step(X)
+        assert isinstance(out, dict)
+        assert isinstance(out["mean"], np.ndarray)
+        assert out["mean"].shape[0] == 5
+
+    def test_ensemble_trainer_test(self, ensemble_model: DeepEnsembleModel) -> None:
+        """Test ensemble test step with lightning Trainer."""
+        datamodule = ToyHeteroscedasticDatamodule()
+        trainer = Trainer(
+            log_every_n_steps=1,
+            max_epochs=1,
+            default_root_dir=ensemble_model.hparams.save_dir,
+        )
+        trainer.test(ensemble_model, datamodule.test_dataloader())
