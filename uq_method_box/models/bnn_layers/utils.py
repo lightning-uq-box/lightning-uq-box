51bba5d2af7b76182eb7a4f91f51606fc109866b
Stefan Depeweg <stdepewe@gmail.com>
2023-05-24 17:38:15 +0200

    summarize log_f_hat calculation for batched and iterative mode

--- uq_method_box/models/bnn_layers/utils.py
+++ uq_method_box/models/bnn_layers/utils.py
@@ -22,32 +22,13 @@ def calc_log_f_hat(w: Tensor, m_W: Tensor, std_W: Tensor, prior_sigma: float) ->
     # natural parameters: -1/(2 sigma^2), mu/(sigma^2)
     # \lambda is (\lambda_q - \lambda_prior) / N
     # assuming prior mean is 0 and moving N calculation outside
-    return (
+    out =  (
         ((v_W - prior_sigma) / (2 * prior_sigma * v_W)) * (w**2) + (m_W / v_W) * w
-    ).sum()
+    )
+    # sum out over all dimension except first
 
-
-def calc_log_f_hat_batched(
-    w: Tensor, m_W: Tensor, std_W: Tensor, prior_sigma: float
-) -> Tensor:
-    """Compute single summand in equation 3.16.
-
-    Args:
-        w: weight matrix [num_samples, num_params]
-        m_W: mean weight matrix at current iteration [num_params]
-        std_W: sigma weight matrix at current iteration [num_params]
-        prior_sigma: Prior variance of weights
-
-    Returns:
-        log f hat summed over the parameters shape 0
-    """
-    v_W = std_W**2
-    # natural parameters: -1/(2 sigma^2), mu/(sigma^2)
-    # \lambda is (\lambda_q - \lambda_prior) / N
-    # assuming prior mean is 0 and moving N calculation outside
-    return (
-        ((v_W - prior_sigma) / (2 * prior_sigma * v_W)) * (w**2) + (m_W / v_W) * w
-    ).sum(1)
+    return  torch.sum(out, dim=tuple(range(1, out.ndim)))
+ 
 
 
 def calc_log_normalizer(m_W: Tensor, std_W: Tensor) -> Tensor:
