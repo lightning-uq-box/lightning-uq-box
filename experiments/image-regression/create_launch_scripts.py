705bb0658617ce1a36d3abf19f3f39bdc67a73d1
Nils Lehmann <nils.lehmann24@gmail.com>
2023-07-07 09:27:32 +0200

    launch script steers directory and wandb

--- experiments/image-regression/create_launch_scripts.py
+++ experiments/image-regression/create_launch_scripts.py
@@ -2,21 +2,26 @@ import itertools
 import os
 import stat
 
-GPUS = [0, 1, 2, 3, 0, 1, 2]
-CONF_FILE_NAMES = ["base.yaml", "gaussian_nll.yaml", "mc_dropout.yaml", "quantile_regression.yaml", "der.yaml", "dkl.yaml", "bnn_elbo.yaml"]
+GPUS = [0, 1, 2, 3, 0, 1, 2, 3]
+CONF_FILE_NAMES = ["base.yaml", "gaussian_nll.yaml", "mc_dropout.yaml", "quantile_regression.yaml", "der.yaml", "dkl.yaml", "due.yaml", "bnn_elbo.yaml"]
 CONF_BASE_DIR = (
-    "/p/project/hai_uqmethodbox/nils/uq-method-box/experiments/image-regression/configs/usa_vars"
+    "/p/project/hai_uqmethodbox/nils/uq-method-box/experiments/image-regression/configs/usa_vars_features_extracted"
 )
 SEEDS = [0]
+
+OOD = True
 
 if __name__ == "__main__":
     for idx, (seed, conf_name) in enumerate(
         itertools.product(SEEDS, CONF_FILE_NAMES)
     ):
         config_file = os.path.join(CONF_BASE_DIR, conf_name)
-
+        if OOD:
+            run_command = "python run_usa_vars_ood.py"
+        else:
+            run_command = "python run_usa_vars_exp.py"
         command = (
-            "python run_usa_vars_exp.py"
+            run_command
             + f" config_file={config_file}"
             + f" experiment.seed={seed}"
             + " trainer.devices=[0]"
@@ -26,8 +31,20 @@ if __name__ == "__main__":
         if base_dir == "usa_vars":
             command += " datamodule.root=/dev/shm/usa_vars/"
             command += " default_config=/p/project/hai_uqmethodbox/nils/uq-method-box/experiments/image-regression/configs/usa_vars/default.yaml"
+            if OOD:
+                command += " experiment.exp_dir=/p/project/hai_uqmethodbox/experiment_output/usa_vars_resnet_ood/"
+                command += " wandb.project=usa_vars_resnet_ood"
+            else:
+                command += " experiment.exp_dir=/p/project/hai_uqmethodbox/experiment_output/usa_vars_resnet/"
+                command += " wandb.project=usa_vars_resnet"
         else:
             command += " default_config=/p/project/hai_uqmethodbox/nils/uq-method-box/experiments/image-regression/configs/usa_vars_features_extracted/default.yaml"
+            if OOD:
+                command += " experiment.exp_dir=/p/project/hai_uqmethodbox/experiment_output/usa_vars_reproduce_ood/"
+                command += " wandb.project=usa_vars_reproduce_ood"
+            else:
+                command += " experiment.exp_dir=/p/project/hai_uqmethodbox/experiment_output/usa_vars_reproduce/"
+                command += " wandb.project=usa_vars_reproduce"
 
         command = command.strip()
 
