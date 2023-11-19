589f8a89fe3b36d10faa6cf1f56295de8c891798
Nils Lehmann <nils.lehmann24@gmail.com>
2023-11-19 14:21:28 +0100

    try dependencies of pyproject.toml

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name="lightning_uq_box",
-    version="0.0.1",
-    description="Description of your package",
-    packages=find_packages(),
-)
