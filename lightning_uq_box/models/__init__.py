e85baa3a431c670487abd0bbd795f34071d27365
Nils Lehmann <nils.lehmann24@gmail.com>
2023-11-24 11:10:55 +0100

    forgot a card file and flipout tests

--- lightning_uq_box/models/__init__.py
+++ lightning_uq_box/models/__init__.py
@@ -1,18 +1,8 @@
 """UQ-Regression-Box Models."""
 
-from .cards import (
-    ConditionalGuidedConvModel,
-    ConditionalGuidedLinearModel,
-    ConditionalLinear,
-)
 from .mlp import MLP
 
 __all__ = (
-    # custom models
-    # CARDS
-    "ConditionalLinear",
-    "ConditionalGuidedLinearModel",
-    "ConditionalGuidedConvModel",
     # Toy Example architecture
     "MLP",
 )
