8ce5e930967f2cb2f51db57becd9da022e2b4568
nilsleh <nils.lehmann24@gmail.com>
2023-12-04 19:09:55 +0000

    store work

--- docs/tutorials/classification/raps.py
+++ docs/tutorials/classification/raps.py
@@ -1,24 +1,26 @@
-import torch
+import os
 from typing import Sequence
 
 import matplotlib.pyplot as plt
 import numpy as np
+import torch
+import torch.backends.cudnn as cudnn
 from lightning import Trainer
 from lightning.pytorch import seed_everything
-
-from lightning_uq_box.uq_methods import RAPS
+from orig_conformal import ConformalModel
+from orig_utils import (
+    AverageMeter,
+    compute_accuracy,
+    coverage_size,
+    get_model,
+    validate,
+)
 from tqdm import tqdm
 
-from orig_utils import compute_accuracy, coverage_size, get_model, validate
-
-
-from orig_utils import validate
-from orig_conformal import ConformalModel
-
-from orig_utils import AverageMeter, coverage_size, compute_accuracy
-import torch.backends.cudnn as cudnn
+from lightning_uq_box.uq_methods import RAPS
 
 cudnn.benchmark = True
+import pandas as pd
 
 plt.rcParams["figure.figsize"] = [14, 5]
 
@@ -40,9 +42,10 @@ def compute_empirical_coverage(S: list[Sequence[int]], y: list[int]) -> float:
 seed_everything(0)
 my_temp_dir = "."
 
-# Imagenet datamodule
-from torchvision import transforms, datasets
 from torch.utils.data import DataLoader, random_split
+
+# Imagenet datamodule
+from torchvision import datasets, transforms
 
 
 class CustomImageFolder(datasets.ImageFolder):
@@ -80,8 +83,8 @@ cal_size = len(dataset) - val_size
 val_dataset, cal_dataset = random_split(dataset, [val_size, cal_size])
 
 # Create the DataLoaders
-batch_size = 128
-num_workers = 8
+batch_size = 256
+num_workers = 12
 val_dataloader = DataLoader(
     val_dataset, batch_size=batch_size, num_workers=num_workers, shuffle=False
 )
@@ -92,38 +95,17 @@ cal_dataloader = DataLoader(
 their_cal_loader = DataLoader(
     cal_dataset,
     batch_size=batch_size,
-    num_workers=8,
+    num_workers=num_workers,
     shuffle=False,
     collate_fn=collate_fn,
 )
 their_val_loader = DataLoader(
     val_dataset,
     batch_size=batch_size,
-    num_workers=8,
+    num_workers=num_workers,
     shuffle=False,
     collate_fn=collate_fn,
 )
-print(len(val_dataloader), len(cal_dataloader))
-# y_true, y_pred = [], []
-# for batch in tqdm(their_cal_loader):
-#     image = batch[0].to(device)
-#     # label = batch["label"].to(device)
-#     with torch.no_grad():
-#         y_true.append(batch[1])
-#         y_pred.append(timm_model(image).cpu())
-
-# y_true = torch.cat(y_true)
-# y_pred = torch.cat(y_pred)
-
-# print("DEFAULT MODEL")
-# # Compute accuracy
-# accuracy = (torch.argmax(y_pred, dim=1) == y_true).float().mean()
-# print(accuracy)
-# # compute coverage
-# S_pred = [set(torch.topk(y_pred[i], 2).indices.tolist()) for i in range(y_pred.shape[0])]
-# coverage = compute_empirical_coverage(S_pred, y_true)
-# print(coverage)
-
 
 # ## Model
 # Specify the device
@@ -154,7 +136,7 @@ def fit_original_raps(model: torch.nn.Module):
     )
 
     print("Model calibrated and conformalized! Now evaluate over remaining data.")
-    validation_metrics = validate(their_cal_loader, model, print_bool=True)
+    validation_metrics = validate(their_val_loader, model, print_bool=True)
 
     return model, {
         "top1": validation_metrics[0],
@@ -178,7 +160,6 @@ def fit_my_raps(orig_model: torch.nn.Module):
     )
     # need to pass the calibration loader here because that is the dataset over which Q_hat is computed
     raps_trainer.validate(raps, dataloaders=cal_dataloader)
-    raps.temperature = orig_model.T
 
     # now adjust the logits
     raps = raps.to(device)
@@ -186,7 +167,8 @@ def fit_my_raps(orig_model: torch.nn.Module):
     top5 = AverageMeter("top5")
     coverage = AverageMeter("RAPS coverage")
     size = AverageMeter("RAPS size")
-    for batch in tqdm(cal_dataloader):
+    # evaluate on full validation set
+    for batch in tqdm(val_dataloader):
         image = batch["image"].to(device)
         with torch.no_grad():
             out = raps.predict_step(image)
@@ -210,32 +192,30 @@ def fit_my_raps(orig_model: torch.nn.Module):
         "top5": top5.avg,
         "coverage": coverage.avg,
         "size": size.avg,
-        "temperature": raps.temperature.detach().item()
+        "temperature": raps.temperature.detach().item(),
     }
 
 
-result_dict = {}
-model_names = ["ResNet18", "ResNet50", "ResNet101", "ResNet152"]
+if __name__ == "__main__":
+    num_seeds = 10
+    for i in tqdm(range(num_seeds)):
+        print(f"RUNING SEED {i}")
+        seed_everything(i)
+        result_dict = {}
+        model_names = ["ResNet18", "ResNet50", "ResNet101", "ResNet152"]
 
-for name in model_names:
-    result_dict[name] = {}
-    model = get_model(name)
+        for name in model_names:
+            result_dict[name] = {}
+            model = get_model(name)
 
-    conformal_model, orig_metrics = fit_original_raps(model)
+            conformal_model, orig_metrics = fit_original_raps(model)
 
-    raps, metrics = fit_my_raps(conformal_model)
-    result_dict[name]["orig"] = orig_metrics
-    result_dict[name]["my"] = metrics
+            raps, metrics = fit_my_raps(conformal_model)
+            result_dict[name]["orig"] = orig_metrics
+            result_dict[name]["my"] = metrics
 
-print(result_dict)
-
-import pandas as pd
-df = pd.DataFrame(result_dict)
-df = df.stack()
-df = df.apply(pd.Series)
-df = df.swaplevel(0, 1).sort_index()
-
-print(df)
-import pdb
-pdb.set_trace()
-print(0)
+        df = pd.DataFrame(result_dict)
+        df = df.stack()
+        df = df.apply(pd.Series)
+        df = df.swaplevel(0, 1).sort_index()
+        df.to_csv(os.path.join("results", f"imagenet_results_{i}.csv"), index=False)
