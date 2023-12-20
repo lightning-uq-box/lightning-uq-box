69682fb2a3609d1f04e797dc9bbfcf984437ec4d
Nils Lehmann <nils.lehmann24@gmail.com>
2023-12-20 17:36:10 +0100

    add sampling during prediction

--- lightning_uq_box/uq_methods/prob_unet.py
+++ lightning_uq_box/uq_methods/prob_unet.py
@@ -33,7 +33,7 @@ from torch.distributions import kl
 from lightning_uq_box.models import AxisAlignedConvGaussian, Fcomb
 from lightning_uq_box.uq_methods import BaseModule
 
-from .utils import default_segmentation_metrics
+from .utils import default_segmentation_metrics, process_segmentation_prediction
 
 
 class ProbUNet(BaseModule):
@@ -55,6 +55,7 @@ class ProbUNet(BaseModule):
         num_convs_fcomb: int = 4,
         fcomb_filter_size: int = 32,
         beta: float = 10.0,
+        num_samples: int = 5,
         task: str = "multiclass",
         optimizer: OptimizerCallable = torch.optim.Adam,
         lr_scheduler: Optional[LRSchedulerCallable] = None,
@@ -69,6 +70,7 @@ class ProbUNet(BaseModule):
             num_convs_fcomb: number of convolutions in fcomb
             fcomb_filter_size: filter size for the fcomb network
             beta: beta parameter
+            num_samples: number of latent samples to use during prediction
             task: task type, either "multiclass" or "binary"
             optimizer: optimizer
             lr_scheduler: learning rate scheduler
@@ -80,6 +82,7 @@ class ProbUNet(BaseModule):
         self.num_filters = num_filters
         self.fcomb_filter_size = fcomb_filter_size
         self.num_convs_per_block = num_convs_per_block
+        self.num_samples = num_samples
 
         assert task in self.valid_tasks, f"Task must be one of {self.valid_tasks}."
         self.task = task
@@ -229,6 +232,23 @@ class ProbUNet(BaseModule):
                 z_posterior = self.posterior_latent_space.rsample()
         return self.fcomb.forward(self.unet_features, z_posterior)
 
+    def sample(self, testing: bool = False) -> Tensor:
+        """Sample a segmentation via reconstructing from a prior sample.
+
+        Args:
+            testing: whether to sample from the prior or use the mean
+        """
+        if testing is False:
+            z_prior = self.prior_latent_space.rsample()
+            self.z_prior_sample = z_prior
+        else:
+            # You can choose whether you mean a sample or the mean here.
+            # For the GED it is important to take a sample.
+            # z_prior = self.prior_latent_space.base_dist.loc
+            z_prior = self.prior_latent_space.sample()
+            self.z_prior_sample = z_prior
+        return self.fcomb.forward(self.unet_features, z_prior)
+
     def training_step(
         self, batch: dict[str, Tensor], batch_idx: int, dataloader_idx: int = 0
     ) -> Tensor:
@@ -279,7 +299,7 @@ class ProbUNet(BaseModule):
 
     def test_step(
         self, batch: dict[str, Tensor], batch_idx: int, dataloader_idx: int = 0
-    ) -> Tensor:
+    ) -> Dict[str, Tensor]:
         """Compute and return the test loss.
 
         Args:
@@ -290,22 +310,36 @@ class ProbUNet(BaseModule):
         Returns:
             test prediction dict
         """
-        pass
+        preds = self.predict_step(batch[self.input_key])
+
+        # compute metrics with sampled reconstruction
+        self.test_metrics(preds, batch[self.target_key])
+
+        return preds
 
     def predict_step(
-        self, X: Tensor, batch_idx: int, dataloader_idx: int = 0
-    ) -> Tensor:
+        self, X: Tensor, batch_idx: int = 0, dataloader_idx: int = 0
+    ) -> Dict[str, Tensor]:
         """Compute and return the prediction.
 
         Args:
-            X: the input data
+            X: the input image
             batch_idx: the index of the batch
             dataloader_idx: the index of the dataloader
 
         Returns:
             prediction dict
         """
-        pass
+        # this internally computes the latent space and unet features
+        self.prior_latent_space = self.prior.forward(X)
+        self.unet_features = self.model.forward(X)
+
+        # which can then be used to sample a segmentation
+        samples = torch.stack(
+            [self.sample(testing=True) for _ in range(self.num_samples)], dim=-1
+        )  # shape: (ba tch_size, num_classes, height, width, num_samples)
+
+        return process_segmentation_prediction(samples)
 
     def configure_optimizers(self) -> dict[str, Any]:
         """Initialize the optimizer and learning rate scheduler.
