ebe58223db9e12f310de52b02ec150a77db69dc1
Nils Lehmann <nils.lehmann24@gmail.com>
2024-01-24 11:40:55 +0100

    improve test coverage

--- tests/uq_methods/test_regression.py
+++ tests/uq_methods/test_regression.py
@@ -22,6 +22,7 @@ model_config_paths = [
     "tests/configs/regression/mean_variance_estimation.yaml",
     "tests/configs/regression/qr_model.yaml",
     "tests/configs/regression/conformal_qr.yaml",
+    "tests/configs/regression/conformal_qr_with_module.yaml",
     "tests/configs/regression/der.yaml",
     "tests/configs/regression/bnn_vi_elbo.yaml",
     "tests/configs/regression/bnn_vi.yaml",
