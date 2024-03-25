4976850840283152f9a19cb7fed82cb467472675
Nils Lehmann <nils.lehmann24@gmail.com>
2024-03-25 13:59:48 +0100

    not necessary

--- lightning_uq_box/models/img2img_conformal/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2023 lightning-uq-box. All rights reserved.
-# Licensed under the Apache License 2.0.
-
-"""Img2Img Conformal Models UQ Layers."""
-
-from .quantile_regression import QuantileLoss, QuantileRegressionLayer
-
-__all__ = ("QuantileRegressionLayer", "QuantileLoss")
