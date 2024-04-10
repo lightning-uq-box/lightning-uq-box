768fb513145f052d78427ff51cf4c5d2d6a4f616
nm19000 <118897012+nm19000@users.noreply.github.com>
2024-04-10 15:01:48 +0200

    adf for uq propagation

---
+++ lightning_uq_box/models/adf_layers/__init__.py
@@ -0,0 +1,28 @@
+"""ADF Layers."""
+
+from .adf_layers import (
+    AvgPool2d_adf,
+    ConvTranspose2d_adf,
+    Conv2d_adf,
+    BatchNorm2d_adf,
+    Linear_adf,
+    MaxPool2d_adf,
+    Dropout_adf,
+    LeakyReLU_adf,
+    ReLU_adf,
+    Softmax_adf,
+)
+
+
+__all__ = (
+    "AvgPool2d_adf",
+    "ConvTranspose2d_adf",
+    "Conv2d_adf",
+    "BatchNorm2d_adf",
+    "Linear_adf",
+    "MaxPool2d_adf",
+    "Dropout_adf",
+    "LeakyReLU_adf",
+    "ReLU_adf",
+    "Softmax_adf",
+)
