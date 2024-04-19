61f1aa54501d96f745a6b18aad52e87d0b3462bb
qqaazz800624 <qqaazz800624@gmail.com>
2024-04-19 10:17:43 +0000

    Fix the loss function in probabilistic UNet.js

--- lightning_uq_box/uq_methods/prob_unet.py
+++ lightning_uq_box/uq_methods/prob_unet.py
@@ -179,7 +179,7 @@ class ProbUNet(BaseModule):
         rec_loss_sum = torch.sum(rec_loss)
         rec_loss_mean = torch.mean(rec_loss)
 
-        loss = -(rec_loss_sum + self.beta * kl_loss)
+        loss = (rec_loss_sum + self.beta * kl_loss)
 
         return {
             "loss": loss,
