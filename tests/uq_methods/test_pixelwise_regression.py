fd87559619e52bc927f1e67ae05537171218277c
Nils Lehmann <nils.lehmann24@gmail.com>
2024-04-26 15:07:33 +0200

    add cpu to other trainers

--- tests/uq_methods/test_pixelwise_regression.py
+++ tests/uq_methods/test_pixelwise_regression.py
@@ -170,7 +170,7 @@ class TestDeepEnsemble:
             len(ensemble_members_dict), ensemble_members_dict
         )
         datamodule = ToyPixelwiseRegressionDataModule()
-        trainer = Trainer(default_root_dir=str(tmp_path))
+        trainer = Trainer(accelerator="cpu", default_root_dir=str(tmp_path))
         trainer.test(ensemble_model, datamodule=datamodule)
 
         # check that predictions are saved
