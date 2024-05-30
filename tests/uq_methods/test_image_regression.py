46ca758e6b7006908b14650ff482a1a5eea24089
Nils Lehmann <nils.lehmann24@gmail.com>
2024-05-30 12:18:36 +0200

    coverage

--- tests/uq_methods/test_image_regression.py
+++ tests/uq_methods/test_image_regression.py
@@ -36,7 +36,8 @@ model_config_paths = [
     "tests/configs/image_regression/laplace_nn.yaml",
     "tests/configs/image_regression/cards.yaml",
     "tests/configs/image_regression/sngp.yaml",
-    "tests/configs/image_regression/vbll.yaml",
+    "tests/configs/image_regression/vbll_replace.yaml",
+    "tests/configs/image_regression/vbll_attach.yaml",
 ]
 
 data_config_paths = ["tests/configs/image_regression/toy_image_regression.yaml"]
