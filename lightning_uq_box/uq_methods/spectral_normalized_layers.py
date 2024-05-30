f06819a62d5c5f181e6b86c9b928cc5a93ac794c
Nils Lehmann <nils.lehmann24@gmail.com>
2024-05-30 13:48:12 +0200

    spectral bug

--- lightning_uq_box/uq_methods/spectral_normalized_layers.py
+++ lightning_uq_box/uq_methods/spectral_normalized_layers.py
@@ -314,7 +314,7 @@ class SpectralNormConv(SpectralNorm):
 
         # get settings from conv-module (for transposed convolution parameters)
         stride = module.stride
-        padding = module.padding
+        padding = module._reversed_padding_repeated_twice[0:2]
 
         if do_power_iteration:
             with torch.no_grad():
