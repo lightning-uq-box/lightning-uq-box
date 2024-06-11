b3efaca4ecdaeeda5f74a5ddfad87fbfc620bb9c
Nils Lehmann <nils.lehmann24@gmail.com>
2024-06-11 20:59:26 +0200

    more tests

--- tests/uq_methods/test_image_regression.py
+++ tests/uq_methods/test_image_regression.py
@@ -38,6 +38,7 @@ model_config_paths = [
     "tests/configs/image_regression/sngp.yaml",
     "tests/configs/image_regression/vbll_replace.yaml",
     "tests/configs/image_regression/vbll_attach.yaml",
+    "tests/configs/image_regression/masked_ensemble.yaml",
 ]
 
 data_config_paths = ["tests/configs/image_regression/toy_image_regression.yaml"]
